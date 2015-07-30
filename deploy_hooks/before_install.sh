6bc9d87576f86233ec2a53694779f3fea450c8c3
Adam <guernica0131@gmail.com>
2015-07-30 11:43:56 +0500

    Updated install hook with -y

--- deploy_hooks/before_install.sh
+++ deploy_hooks/before_install.sh
@@ -1,8 +1,8 @@
 curl -sL https://deb.nodesource.com/setup | bash -
 
 apt-get update
-apt-get install nodejs
-apt-get install build-essential
+apt-get install -y nodejs
+apt-get install -y build-essential
 
 npm install sails -g
 npm install bower -g
